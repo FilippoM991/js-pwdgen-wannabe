1de2f18c43ee4b29e99a0a1ea908c0529f869586
Maran Filippo <filippo.maran.fm91@gmail.com>
2019-11-13 16:54:02 +0100

    aggiunta ape

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+alert('Tre facili domande ti separano da una password nuova fiammante!')
 var nome = prompt('Qual è il tuo nome?')
 var cognome = prompt('Qual è il tuo cognome?')
 var colore = prompt('Qual è il tuo colore preferito?')
